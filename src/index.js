a7af52484883f91251fed71559c355f199741992
Fortune Ekeruo <legalmody@gmail.com>
2019-03-19 16:46:42 +0100

    Create root node component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-alert('Hello World');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'Delivery Service App';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
