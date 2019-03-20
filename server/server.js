e08bfa20f1ef2f1c5a2870f1316cbe2e66266e86
Fortune Ekeruo <legalmody@gmail.com>
2019-03-20 22:34:46 +0100

    Users mock data and implement login with jwt on the server

--- server/server.js
+++ server/server.js
@@ -1,14 +1,40 @@
-const express = require("express");
-const logger = require("morgan");
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config();
+
+const users = require('./mocks/users.js');
 
 const API_PORT = 3001;
 const app = express();
 const router = express.Router();
 
+const secret = process.env.JWT_SECRET;
+
 app.use(logger("dev"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 router.post("/login", (req, res) => {
-  // login user
+    const { email, password } = req.body;
+
+    const user = users.find((user) => user.email === email);
+
+    if (user && user.password === password) {
+        const token = jwt.sign({
+            userId: user.id,
+            name: user.name,
+            email: user.email,
+            role: user.role
+          }, secret, { expiresIn: '2 days' });
+  
+        res.status(200).send({ token, expiresIn: '2 days' });
+    } else {
+        res.status(401)
+          .send({ message: 'Authentication failed due to invalid credentials.' });
+    }
 });
 
 router.get("/shipment", (req, res) => {
