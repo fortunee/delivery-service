c89a6dcca5e32ead0f725a7077a5e147e5a44e3b
Fortune Ekeruo <legalmody@gmail.com>
2019-03-21 11:08:58 +0100

    Create a logged out state component and add it to the Navbar component

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import LoggedInLinks from './LoggedInLinks';
+import LoggedOutLinks from './LoggedOutLinks';
 
 const Navbar = () => {
     return (
@@ -8,6 +9,7 @@ const Navbar = () => {
             <div className="container">
                 <Link to="/" className="brand-logo">Monica Delivery Service</Link>
                 <LoggedInLinks />
+                <LoggedOutLinks />
             </div>
         </nav>
     )
