8d2f137c4ceb8808c7c0500757f47e9873bec337
Fortune Ekeruo <legalmody@gmail.com>
2019-03-23 20:40:29 +0100

    Add mock data for shipments

---
+++ server/mocks/shipments.js
@@ -0,0 +1,47 @@
+module.exports = [
+    {
+        id: 1,
+        parcel: 'Tshirt',
+        timestamp: new Date().toLocaleString("en-US"),
+        origin: 'Cologne',
+        destination: 'Lagos',
+        assignee: 'Unassigned',
+        order_status: 'Waiting'
+      },
+      {
+        id: 2,
+        parcel: 'Hat',
+        timestamp: new Date().toLocaleString("en-US"),
+        origin: 'Berlin',
+        destination: 'Abuja',
+        assignee: 'John',
+        order_status: 'Picked up'
+      },
+      {
+        id: 3,
+        parcel: 'Sneakers',
+        timestamp: new Date().toLocaleString("en-US"),
+        origin: 'Hamburg',
+        destination: 'Cologne',
+        assignee: 'Moritz',
+        order_status: 'Deliverd'
+      },
+      {
+        id: 4,
+        parcel: 'Shoes',
+        timestamp: new Date().toLocaleString("en-US"),
+        origin: 'Cologne',
+        destination: 'New York',
+        assignee: 'Michael',
+        order_status: 'Assigned'
+      },
+      {
+        id: 5,
+        parcel: 'Apple watch',
+        timestamp: new Date().toLocaleString("en-US"),
+        origin: 'Hambur',
+        destination: 'Lagos',
+        assignee: 'Maurice',
+        order_status: 'Delieverd'
+      }
+  ]
