79ecdb351e7e081f65c94993eb7811cc8eacf80e
Fortune Ekeruo <legalmody@gmail.com>
2019-03-25 11:23:02 +0100

    Display shipment in materialize collection

--- client/src/components/shipment/ShipmentList.js
+++ client/src/components/shipment/ShipmentList.js
@@ -4,27 +4,11 @@ import ShipmentDetails from './ShipmentDetails';
 const ShipmentList = ({ shipments }) => {
     return (
         <div className="shipment-list">
-            <table className="highlight responsive-table white">
-                <thead>
-                <tr>
-                    <th>Parcel</th>
-                    <th>Origin</th>
-                    <th>Destination</th>
-                    <th>Assignee</th>
-                    <th>Order Status</th>
-                    <th>Time Stamp</th>
-                    <th>Edit</th>
-                </tr>
-                </thead>
-
-                <tbody>
-                    { shipments && shipments.map(shipment => {
-                        return (
-                            <ShipmentDetails  shipment={shipment} key={shipment.id} />
-                        )
-                    })}
-                </tbody>
-            </table>
+            { shipments && shipments.map(shipment => {
+                return (
+                    <ShipmentDetails  shipment={shipment} key={shipment.id} />
+                )
+            })}
         </div>
     );
 }
