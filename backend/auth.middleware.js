54f235ecfff57c4a50f15ffe63553b4135c7c6d1
Fortune Ekeruo <legalmody@gmail.com>
2019-05-06 23:37:46 +0100

    get rid of unnecessary comments

--- backend/auth.middleware.js
+++ backend/auth.middleware.js
@@ -4,14 +4,6 @@ const users = require('./mocks/users.js');
 const secret = process.env.JWT_SECRET || 'SECRET TEXT';
 
 const Auth = {
-  /**
-   * verifyToken - Verifies validity of a supplied token
-   *
-   * @param  {object} req  Request object
-   * @param  {object} res  Response object
-   * @param  {object} next
-   * @returns {object} Response status
-   */
   verifyToken(req, res, next) {
     const token = req.headers.authorization || req.headers['x-access-token'];
     if (!token) {
@@ -31,14 +23,6 @@ const Auth = {
     });
   },
 
-  /**
-   * verifyManager - Verifies that user is a manager
-   *
-   * @param  {object} req  Request object
-   * @param  {object} res  Response object
-   * @param  {object} next
-   * @returns {object} Response object
-   */
   verifyManager(req, res, next) {
     const { role } = req.decoded;
     const user = users.find((user) => user.role === role);
